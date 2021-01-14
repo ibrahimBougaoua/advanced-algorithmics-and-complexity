6e77c8b0b13cefd88a6d7435db50992967475204
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2021-01-14 18:39:58 -0500

    Refactoring : Algorithme 1

--- TP1 Primality Test/Algorithme 1/main.c
+++ TP1 Primality Test/Algorithme 1/main.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 #include <time.h>
 
-int primeNumber(int n)
+// f(n) = 1 + 3 + 1 + 1 + 1 + n - 1 + 2*(n-2) + 1 + n - 2 + 1 + 1 + 1
+// f(n) = 4n + 4
+
+void primeNumber(int n)
 {
-	int i = 2;
+	int i = 2; // 1
 	
-	if( (n == 0) || (n == 1) )
-	   return -1;
+	if( (n == 0) || (n == 1) ) // 3 
+	   printf("This isn't a prime number"); // 1
 	    
-	if( n == 2 )
-	   return 1;
+	if( n == 2 ) // 1
+	   printf("This is a prime number"); // 1
 	
-	while( (i < n) && (n % i) != 0 )
+	while( i < n ) // n-1
 	{
-		i++;
+		if((n % i) == 0) // 2*(n-2)
+			break; // 1
+		i++; // n-2
 	}
 	
-	if( i == n )
-	  return 1;
-	  
-	return -1;
+	if( i == n ) // 1
+	  printf("This is a prime number"); // 1
+	else
+	  printf("This isn't a prime number"); // 1
 }
 
 int main()
@@ -32,11 +37,8 @@ int main()
     // Calculate the time spent by primeNumber(n) 
     clock_t begin = clock();
     
-	if( primeNumber(n) == 1 )
-	    printf("This is a prime number");
-	else
-	    printf("This isn't a prime number");
-	
+	primeNumber(n);
+	    
 	clock_t end = clock();
     
 	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
