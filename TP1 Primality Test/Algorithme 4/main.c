03f6e313ecc3b47320181bdb274ebef01c9e85f2
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2021-01-16 18:22:24 -0500

    Refactoring : Algorithme 4

--- TP1 Primality Test/Algorithme 4/main.c
+++ TP1 Primality Test/Algorithme 4/main.c
@@ -1,38 +1,23 @@
 #include <stdio.h>
 #include <time.h>
 
-int primeNumber(int n)
-{
-	int i = 3;
-	
-	if( (n == 0) || (n == 1) )
-	   return -1;
-	    
-	if( n == 2 )
-	   return 1;
-	
-	if((n % 2) != 0)
-	{
-		while( (i < n) && (n % i) != 0 )
-		{
-			i += 2;
-		}
-	} else {
-		while( (i < n) && (n % i) != 0 )
-		{
-			i++;
-		}
-	}
-	
-	if( i == n )
-	  return 1;
-	  
-	return -1;
-}
+// Au pire cas :
+// f(n) = 1 + 1 + 1 + 2 + [n^(1/2) + 1]/2 + 2*(n^(1/2)) + 2*(n^(1/2)) + n^(1/2)
+// f(n) = 11*n^(1/2) + 13/2
+
+// T(n) = (11*n^(1/2) + 13/2)*Dt
+// Dt = T(n)/(11*n^(1/2) + 13/2)
+
+// Au meilleur cas :
+// f(n) = 1 + 1 + 1 + 2
+// f(n) = 5
+
+// T(n) = 5*Dt
+// Dt = T(n)/5
 
 int main()
 {
-	int n;
+	int n,i,nbr,pr; 
 	
 	printf("Enter you'r number : ");
 	scanf("%d",&n);
@@ -40,18 +25,36 @@ int main()
     // Calculate the time spent by primeNumber(n) 
     clock_t begin = clock();
     
-	if( primeNumber(n) == 1 )
-	    printf("This is a prime number");
-	else
-	    printf("This isn't a prime number");
+	i = 1; // 1
+	nbr = 0; // 1
+	pr = 1; // 1
+	
+	if((n % 2) != 0) // 2
+	{
+		while( i <= sqrt(n) ) // [n^(1/2) + 1]/2
+		{
+			if((n % i) == 0) // 2*(n^(1/2))
+				nbr++; // 2*(n^(1/2)) or 2*2
+				
+			i = i + 2; // n^(1/2)
+		}
+	}
 	
 	clock_t end = clock();
     
 	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
     
+	if( nbr == 1 )
+	  printf("This is a prime number");
+	else
+	  printf("This isn't a prime number");
+	  
     puts(" ");
   
-    printf("primeNumber(n) took %f seconds to execute \n", time_spent); 
+    printf("Le temps d'execution est temps = %f seconds \n", time_spent); 
 	
 	return 0;
 }
+
+
+
