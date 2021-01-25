09dbab22963b073f3cf66c8533262c362219443c
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2021-01-25 16:33:42 -0500

    Refactoring : Algorithme 2

--- TP2 Search/Algorithme 2/main.c
+++ TP2 Search/Algorithme 2/main.c
@@ -2,50 +2,118 @@
 #include <time.h>
 
 // Au pire cas :
-// f(n) = 1 + 3 + n + 1 + n + 1 + 2*n + 1 + 1
-// f(n) = 4n + 8
+// f(n) = 3 + n + 1 + n + n + n + n + 2*n + 1
+// f(n) = 7n + 5
 
-// T(n) = (4n + 5)*Dt
-// Dt = T(n)/(4n + 5)
+// T(n) = (7n + 5)*Dt
+// Dt = T(n)/(7n + 5)
 
 // Au meilleur cas :
-// f(n) = 1 + 1 + 1 + 1 + 1
-// f(n) = 5
+// f(n) = 3 + 2 + 1 + 1 + 1 + 1 + 2 + 1
+// f(n) = 12
 
-// T(n) = 5*Dt
-// Dt = T(n)/5
+// T(n) = 12*Dt
+// Dt = T(n)/12
 
 void maxEtMinA(int *tab,int n)
 {
-	int i = 1,max = tab[1],min = tab[1]; // 1
+	int i = 1,max = tab[1],min = tab[1]; // 3
 	
-	while( i <= n ) // n + 1 or 1
+	while( i <= n ) // n + 1 or 2
 	{
 		if( tab[i] >= max ) // n or 1
-		    max = tab[i];
+		    max = tab[i]; // n or 1
 		
 		if( tab[i] <= min ) // n or 1
-		    min = tab[i];
+		    min = tab[i]; //n or 1
 		
-		i++; // 2*n or 0
+		i++; // 2*n or 2
 	}
 	
 	printf("Max = %d et Min = %d \n",max,min); // 1
 }
 
+void maxEtMinB(int *tab,int n)
+{
+	int i = 0,cpt = 0,x = 0,max,min; // 3
+	
+	while( i < n-1 ) // n/2
+	{
+		if( tab[i] > tab[i+1] ) // n or 1
+		{
+			cpt++;
+		    x = tab[i];
+		    tab[i] = tab[i+1];
+		    tab[i+1] = x;
+		}
+		
+		i = i + 2; // 2*n or 2
+	}
+	
+	min = tab[0];
+	i = 2;
+	
+	while( i < n ) // n/2-1
+	{
+		cpt++;
+		if( min > tab[i] ) // n or 1
+		{
+		    min = tab[i];
+		}
+		
+		i = i + 2; // 2*n or 2
+	}
+	
+	min = tab[1];
+	i = 3;
+	
+	while( i < n ) // n/2-1
+	{
+		cpt++;
+		if( max < tab[i] ) // n or 1
+		{
+		    max = tab[i];
+		}
+		
+		i = i + 2; // 2*n or 2
+	}
+	
+	if(n%2 == 1)
+	{
+		if( max < tab[n-1] ) // n or 1
+		{
+		    cpt++;
+		    max = tab[n-1];
+		}
+	}
+	
+	printf("Max = %d et Min = %d / Counting = %d \n",max,min,cpt); // 1
+}
+
 int main()
 {
-	int i = 1,n = 100;
+	int i = 0,n = 100;
 	long int *tab;
 	tab = (long int *)malloc(n*sizeof(long int *));
 	
-	while( i <= n )
+	while( i < n )
 	{
-		tab[i] = rand();
+		tab[i] = i+1;
 		i++;
 	}
 	
-	maxEtMinA(tab,n);
+	// Calculate the time spent by function. 
+    clock_t begin = clock();
+    
+	maxEtMinB(tab,n);
+	
+	clock_t end = clock();
+    
+	double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    
+    puts(" ");
+  
+    printf("Le temps d'execution est temps = %f seconds \n", time_spent); 
 	 
 	return 0;
 }
