6393bbb31442a57062f609a90b8c7561aeef51ef
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2021-02-16 15:16:36 -0500

    Refactoring : TriAux function

--- Exam/main.c
+++ Exam/main.c
@@ -13,46 +13,57 @@ int Cle(int x,int i)
 	return chiffre;
 }
 
-void TriAux(int *tab,int n,int k)
+void TriAux(int *tab,int n,int i)
 {
-	int i = 1,x;
-	while( i < n )
+	int k[n],tt[n],j,m,indice;
+	
+	indice = 1;
+	for(j=1;j<=n;j++)
 	{
-		if( Cle(tab[i],k) <= Cle(tab[i+1],k) )
+		k[j] = Cle(tab[j],i);
+	}
+	
+	for(j=0;j<=9;j++)
+	{
+		for(m=1;m<=n;m++)
 		{
-			x = tab[i];
-			tab[i] = tab[i+1];
-			tab[i+1] = x;
+			if(k[m]=j)
+			{
+				indice++;
+			}
 		}
-		i++;
+	}
+	
+	for(m=1;m<=n;m++)
+	{
+		tab[m] = tt[m];
 	}
 }
 
 int main()
 {
-	int n = 5,i = 1; 
+	int n = 8,i = 0; 
 	
 	long int *tab;
 	tab = (long int *)malloc(n*sizeof(long int *));
 	
-	while( i <= n )
-	{
-		printf("%d \n", i+100); 
-		tab[i] = i+100;
-		i++;
-	}
+    tab[1] = 10;
+    tab[2] = 143;
+    tab[3] = 30;
+    tab[4] = 43;
+    tab[5] = 141;
+    tab[6] = 232;
+    tab[7] = 45;
+    tab[8] = 112;
 	
-	puts("");
-	TriAux(tab,n,2);
+    TriAux(tab,n,0);
 	
-	i = 1; 
-	while( i <= n )
+	i = 0; 
+	while( i < n )
 	{
 		printf("%d \n", tab[i]);
 		i++;
 	}
-	
-	printf("x = %d \n", Cle(143,1)); 
 
     // Calculate the time spent by primeNumber(n) 
     clock_t begin = clock();
