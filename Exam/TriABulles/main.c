59ed695cd3241007eece6530c03f55a22e1d4484
Ibrahim <ibrahim.bougaoua@etu.usthb.dz>
2021-02-21 10:26:32 -0500

    Refactoring : TriBulles

--- Exam/TriABulles/main.c
+++ Exam/TriABulles/main.c
@@ -2,88 +2,100 @@
 #include <stdlib.h>
 #include <time.h>
 
-void remplirTable_decroisant(int n, int *t)
+// Fil Up Random
+void FilUpRand(int n, int *t)
 {
     int i;
     for(i=0;i<n;i++)
     {
-	  t[i]=n-i;
+	  t[i] = rand();
     }
 }
 
-  void remplirTable_croisant(int n, int *t)
+// Fil Up Desc
+void FilUpDesc(int n, int *t)
 {
     int i;
     for(i=0;i<n;i++)
-  {
-	  t[i]=i;
-  }
+    {
+	  t[i] = n-i;
+    }
 }
 
-void remplirTable_rand(int n, int *t)
+// Fil Up Asc
+void FilUpAsc(int n, int *t)
 {
     int i;
     for(i=0;i<n;i++)
-  {
-	  t[i]=rand();
-  }
+	{
+		t[i] = i;
+    }
 }
 
-
+// tri Bulle
 void triBulle(int *t,int n){
-    int i =0;int tmp =0;int changement=1;
-    while(changement){
-        changement = 0;
+    int i = 0,x = 0,permut =1;
+    while(permut ){
+        permut = 0;
         for(i =0; i < n-1; i++){
             if(t[i] > t[i+1]){
-                    tmp = t[i+1];
+                    x = t[i+1];
                     t[i+1] = t[i];
-					t[i] = tmp;
-                    changement = 1;
+					t[i] = x;
+                    permut = 1;
             }
         }
     }
 }
 
+// tri Bulle Opt
 void triBulleOpt(int *t,int n){
-	int i =0;int tmp =0;int m;
+	int i = 0,x = 0,m;
 	m=n-1;
-	int changement=1;
-	while(changement){
-		changement = 0;
+	int permut =1;
+	while(permut){
+		permut = 0;
         for(i =0; i < m; i++){
             if(t[i] > t[i+1]){
-                    tmp = t[i+1];
+                    x = t[i+1];
                     t[i+1] = t[i];
-					t[i] = tmp;
-                    changement = 1;
+					t[i] = x;
+                    permut = 1;
             }
         }
 	}
 }
 
+// Main
 int main(int argc, char *argv[])
 {
     int n,i;
     int *t;
-    clock_t Debut,Fin,Debut1,Fin1;
-    float Temp;
 
-  printf("donner la taille N= ");
+  printf("Donner la taille N = ");
   scanf("%d",&n);
-  t=(int *)malloc(n*sizeof(int));
+  t = (int *)malloc(n*sizeof(int));
 
+  FilUpRand(n,t); // fill up table
 
-  remplirTable_rand(n,t);// remplir la table
-
-
-  Debut=clock();
+  // Calculate the time spent by primeNumber(n)
+  clock_t begin = clock();
+  
   triBulle(t,n);
   //triBulleOpt(t,n);
-  Fin=clock();
-  Temp=(float)(Fin-Debut)/CLOCKS_PER_SEC;
-  printf("Le temps d'execution du tri par bull est %f\n",Temp);
+  
+  clock_t end = clock();
+  
+  double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    
+  puts(" ");
+  
+  printf("Le temps d'execution est temps = %f seconds \n", time_spent); 
 
+  for(i=0;i<n;i++)
+  {
+    printf("%d \n",t[i]);
+  }
 
   return 0;
 }
